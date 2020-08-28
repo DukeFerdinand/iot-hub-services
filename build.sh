704ced04392f7922e68e29795483cba68c4803bb
Douglass Hooker <DouglassHooker@gmail.com>
2020-08-28 18:40:23 -0500

    Update builds and make things MUCH MUCH faster with rust cross

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Break if anything exits poorly
+set -xe
+
+echo "Building Auth Server"
+cd iot-auth-server
+
+echo "Compiling for arm64"
+cross build --target aarch64-unknown-linux-gnu --release
+echo "Creating docker image and pushing"
+docker buildx build -t dougflynn/iot-auth-server:latest --platform linux/arm64 -f Dockerfile.prod --push .
+
+echo "DONE. Swapping back to root dir"
+cd ..
+
+echo "Building plant api"
+cd iot-api-plants
+echo "Compiling for arm64"
+cross build --target aarch64-unknown-linux-gnu --release
+echo "Creating docker image and pushing"
+docker buildx build -t dougflynn/iot-api-plants:latest --platform linux/arm64 -f Dockerfile.prod --push .
+
+echo "DONE. Swapping back to root dir"
+cd ..
+
+echo "NOTHING LEFT TO DO :)"
+
+echo "Run the following to push to prod/swarm:"
+echo "git push swarm master"
+
