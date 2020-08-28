385b632ebfb835286e3a1d9337d2098ad06240f1
Douglass Hooker <DouglassHooker@gmail.com>
2020-08-28 08:32:29 -0500

    fix placement of flag

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@ echo "Starting services --------"
 
 # Make sure your images are uploaded before this
 echo "Deploying to swarm"
-/usr/bin/docker stack deploy --compose-file --with-registry-auth docker-compose-prod.yml iot-hub-stack
+/usr/bin/docker stack deploy --with-registry-auth --compose-file docker-compose-prod.yml iot-hub-stack
 
 echo "Done! Please wait a bit for changes to take effect throughout swarm."
