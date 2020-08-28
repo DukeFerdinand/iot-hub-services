8ff2eae60f7fc5e4cee99f1ebab2bc3c9e8d34f6
Douglass Hooker <DouglassHooker@gmail.com>
2020-08-28 09:11:16 -0500

    take down old stack before creating new. okay on a personal project

--- start.sh
+++ start.sh
@@ -2,6 +2,9 @@
 
 echo "Starting services --------"
 
+echo "Removing old stack"
+/usr/bin/docker stack rm iot-hub-stack
+
 # Make sure your images are uploaded before this
 echo "Deploying to swarm"
 /usr/bin/docker stack deploy --with-registry-auth --compose-file docker-compose-prod.yml iot-hub-stack
