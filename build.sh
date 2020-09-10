7fe21e44d6b25f242af57eb63b4a634327634137
Douglass Hooker <DouglassHooker@gmail.com>
2020-09-10 10:02:22 -0500

    remove build keys from prod docker compose, use vars in build script

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+AUTH_SERVER="dougflynn/iot-auth-server:latest"
+IOT_PLANTS="dougflynn/iot-api-plants:latest"
+IOT_MQTT="dougflynn/iot-mqtt-hub:latest"
+
 # Break if anything exits poorly
 set -e
 
@@ -9,7 +13,8 @@ cd iot-auth-server
 echo "Compiling for x86_64"
 cargo build --release
 echo "Creating docker image and pushing"
-docker build -t dougflynn/iot-auth-server:latest -f Dockerfile.prod --push .
+docker build -t $AUTH_SERVER -f Dockerfile.prod .
+docker push $AUTH_SERVER
 
 echo "DONE. Swapping back to root dir"
 cd ..
@@ -19,7 +24,8 @@ cd iot-api-plants
 echo "Compiling for arm64"
 cargo build --release
 echo "Creating docker image and pushing"
-docker build -t dougflynn/iot-api-plants:latest -f Dockerfile.prod --push .
+docker build -t $IOT_PLANTS -f Dockerfile.prod .
+docker push $IOT_PLANTS
 
 echo "DONE. Swapping back to root dir"
 cd ..
@@ -27,7 +33,8 @@ cd ..
 echo "Building MQTT handler"
 cd iot-mqtt-hub
 echo "Creating docker image and pushing"
-docker build -t dougflynn/iot-mqtt-hub:latest -f Dockerfile.prod --push .
+docker build -t $IOT_MQTT -f Dockerfile.prod .
+docker push $IOT_MQTT
 
 # cd iot-mqtt-broker
 # echo "Creating arm64 docker image and pushing"
